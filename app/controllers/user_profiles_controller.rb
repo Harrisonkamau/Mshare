ce74c4fb377b9e0c78b0eeec51cdc6c3c2701305
Harrisonkamau <kamauharrison87@gmail.com>
2018-03-14 12:07:39 +0300

    User registration complete

---
+++ app/controllers/user_profiles_controller.rb
@@ -0,0 +1,10 @@
+class UserProfilesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'user_profiles#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+end
